7f46a9e47b549bbb8b4ec075ba037d074ba410de
papajin <papajini@gmail.com>
2023-08-03 14:15:02 +0300

    Removes redundant classes.

--- boot/config.php
+++ boot/config.php
@@ -5,7 +5,6 @@ return [
     Equip\Configuration\DiactorosConfiguration::class,
 //    Equip\Configuration\MonologConfiguration::class,
     Equip\Configuration\EnvConfiguration::class,
-//    App\Configuration\EnvConfiguration::class,
     Equip\Configuration\PayloadConfiguration::class,
     App\Configuration\PlatesConfiguration::class,
     Equip\Configuration\PlatesResponderConfiguration::class,
