a68f3ab2b2ef37054e4c540bdfa334dc1374b39d
papajin <papajini@gmail.com>
2023-08-03 16:54:58 +0300

    implements class.

--- src/Data/Buyer.php
+++ src/Data/Buyer.php
@@ -2,29 +2,34 @@
 
 namespace App\Data;
 
+use App\Contracts\BuyerInterface;
 use Equip\Data\Traits\EntityTrait;
 
-class Buyer implements \App\Contracts\BuyerInterface
+class Buyer implements BuyerInterface
 {
-    use EntityTrait;
+    use EntityTrait, ArrayAccess;
 
-    public function offsetExists($offset)
-    {
-        // TODO: Implement offsetExists() method.
-    }
+    private $id;
+    private $country_id;
+    private $country_code;
+    private $country_code3;
+    private $shop_username;
+    private $email;
+    private $phone;
+    private $address;
+    private $data;
 
-    public function offsetGet($offset)
+    private function types(): array
     {
-        // TODO: Implement offsetGet() method.
-    }
-
-    public function offsetSet($offset, $value)
-    {
-        // TODO: Implement offsetSet() method.
-    }
-
-    public function offsetUnset($offset)
-    {
-        // TODO: Implement offsetUnset() method.
+        return [
+            'id'            => 'int',
+            'country_id'    => 'int',
+            'country_code'  => 'string',
+            'shop_username' => 'string',
+            'email'         => 'string',
+            'phone'         => 'string',
+            'address'       => 'string',
+            'data'          => 'array',
+        ];
     }
 }
