886e8116c0256f370db582bfb48a3f48609fdd22
papajin <papajini@gmail.com>
2023-08-06 16:21:49 +0300

    Renames class.

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -7,7 +7,7 @@ use Auryn\Injector;
 use Equip\Configuration\ConfigurationSet;
 use PHPUnit\Framework\TestCase;
 
-abstract class FeatureTest extends TestCase
+abstract class BaseTest extends TestCase
 {
     /**
      * @throws InjectionException
