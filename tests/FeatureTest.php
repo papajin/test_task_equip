f44422615b6f168e6b40c62f196fda0ec561fb9d
papajin <papajini@gmail.com>
2023-08-06 15:12:21 +0300

    Creates Feature test base method.

---
+++ tests/FeatureTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Tests;
+
+
+use Auryn\InjectionException;
+use Auryn\Injector;
+use Equip\Configuration\ConfigurationSet;
+use PHPUnit\Framework\TestCase;
+
+class FeatureTest extends TestCase
+{
+    /**
+     * @throws InjectionException
+     */
+    protected function resolve( $name, $args = [] )
+    {
+        return $this->getInjector()->make( $name, $args );
+    }
+
+    protected function getInjector()
+    {
+        static $injector = null;
+
+        if( !$injector instanceof Injector ) {
+            $injector = new Injector;
+            $configs = new ConfigurationSet(require ROOT_PATH . 'boot/config.php');
+            $configs->apply( $injector );
+        }
+
+        return $injector;
+    }
+}
